04f50f8d3f566eaba2326ce46606adb54a0418cf
Tanjila <tanjilaakter723874@gmail.com>
2026-09-24 21:56:14 +0600

    update

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
 
 app.get("/random", (req, res) => {
     res.send("this is a random page");
-})
+});
 
  app.listen(8080, () => {
      console.log("server listening to port 8080");
