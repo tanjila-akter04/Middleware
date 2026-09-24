6fda87d8d7ce47df7e129596aed9b0a46573b1e7
Tanjila <tanjilaakter771746@gmail.com>
2026-09-24 19:01:23 +0600

    new

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 
+app.use( () => {
+    console.log("Hi, I am middleware");
+});
+
+
 app.get("/", (req, res) => {
-    res.send();
+    res.send("Hi, I am, root.");
+});
+
+app.get("/random", (req, res) => {
+    res.send("this is a random page");
 })
 
-// app.listen(8080, () => {
-//     console.log("server listening to port 8080");
-// });
+ app.listen(8080, () => {
+     console.log("server listening to port 8080");
+ });
