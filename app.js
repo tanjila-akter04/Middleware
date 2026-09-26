907bf7aed65c061bdc01a572f21ba2e066447534
Tanjila <tanjilaakter723874@gmail.com>
2026-09-27 00:44:44 +0600

    new

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-    let {query} = req.query;
-   console.log("Hi, I am middleware");
-   next(); 
+// app.use((req, res, next) => {
+//    console.log("Hi, I am 1st middleware");
+//    next(); 
+  
+// });
+
+// app.use((req, res, next) => {
+//    console.log("Hi, I am 2nd middleware");
+//    next(); 
+// });
+
+app.use( (req, res, next) => {
+    console.log(req);
+    next();
 });
 
-
 app.get("/", (req, res) => {
-    res.send("Hi, I am, root.");
+    res.send("Hi, I am root.");
 });
 
 app.get("/random", (req, res) => {
